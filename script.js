b60fe73e831ab6015fad249dfee75e1827cd5ace
KasthuriRangan <krangan415@gmail.com>
2023-08-21 17:31:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+function firstnonrepeatedchar(a)
+	{
+	for(let i=0;i<a.length;i++)
+					 { let char=a[i];
+					 if(a.indexOf(char)==i&& a.indexOf(char,i+1)==-1)
+					 {
+						 return char;
+						 
+					 }else if(a==='')
+					 {
+						 return null;
+					 }else
+					 {
+						 return null
+					 }
+						 }
+	}
+					 
+					 
